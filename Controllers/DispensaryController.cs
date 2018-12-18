54cbc41b40cd46a4afae6a8a009e44344879b070
Guest User <Guest@epicodus-100.local>
2018-12-18 15:03:26 -0800

    Add Violation class

--- Controllers/DispensaryController.cs
+++ Controllers/DispensaryController.cs
@@ -15,15 +15,33 @@ namespace Weed.Controllers
 
         [HttpGet("/dispensaries/{id}")]
         public ActionResult Show(int id)
-        {
+        {   
+            List<Comment> foundComments = Comment.GetCommentsByLicense(id);
             Dispensary foundDispensary = Dispensary.FindByLicense(id);
-            return View(foundDispensary);
+            Dictionary<string, object> model = new Dictionary<string, object> {};
+            model.Add("dispensary", foundDispensary);
+            model.Add("comments", foundComments);
+            return View(model);
         }
 
         [HttpPost("/dispensaries/searchbyname")]
         public ActionResult SearchName(string DispensaryName)
-        {  Dispensary foundDispensary = Dispensary.FindByName(DispensaryName);
-           return View("Show", foundDispensary);     
+        {   
+            Dispensary foundDispensary = Dispensary.FindByName(DispensaryName);
+            return View("Show", foundDispensary);     
+        }
+
+        [HttpPost("/dispensaries/{license}/comments")]
+        public ActionResult CreateComment(int license, string review, int rating)
+        {
+            Comment newComment = new Comment(review, rating, license);
+            newComment.Save();
+            List<Comment> foundComments = Comment.GetCommentsByLicense(license);
+            Dispensary foundDispensary = Dispensary.FindByLicense(license);
+            Dictionary<string, object> model = new Dictionary<string, object> {};
+            model.Add("dispensary", foundDispensary);
+            model.Add("comments", foundComments);
+            return View("Show", model); 
         }
     }
 }
